3984aa7a9f6f76acbfa67c865a487dbfe29fda8f
nguyenvantra <dkdarknessnvt@gmail.com>
2018-06-01 00:55:57 +0700

    init spring-master project

---
+++ springboot-springsecurity-springdatajpa-nosql-mongodb/src/main/java/com/darkness/SpringbootSpringsecuritySpringdatajpaNosqlMongodbApplication.java
@@ -0,0 +1,12 @@
+package com.darkness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringsecuritySpringdatajpaNosqlMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSpringsecuritySpringdatajpaNosqlMongodbApplication.class, args);
+    }
+}

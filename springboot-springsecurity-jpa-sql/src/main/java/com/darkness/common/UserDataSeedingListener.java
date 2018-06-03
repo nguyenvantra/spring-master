b67ee89485e5a0399a55f9e9b929d1451c032087
nguyenvantra <dkdarknessnvt@gmail.com>
2018-06-03 20:44:34 +0700

    Update mock data for application

--- springboot-springsecurity-jpa-sql/src/main/java/com/darkness/common/UserDataSeedingListener.java
+++ springboot-springsecurity-jpa-sql/src/main/java/com/darkness/common/UserDataSeedingListener.java
@@ -10,6 +10,7 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.util.HashSet;
 
 @Component
@@ -23,40 +24,41 @@ public class UserDataSeedingListener implements ApplicationListener<ContextRefre
     @Autowired
     private RoleService roleService;
 
+    @Transactional
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
 
         // Roles
-//        if (roleService.findByName("ROLE_ADMIN") == null) {
-//            roleService.save(new Role("ROLE_ADMIN"));
-//        }
-//
-//        if (roleService.findByName("ROLE_MEMBER") == null) {
-//            roleService.save(new Role("ROLE_MEMBER"));
-//        }
+        if (roleService.findByName("ROLE_ADMIN") == null) {
+            roleService.save(new Role("ROLE_ADMIN"));
+        }
+
+        if (roleService.findByName("ROLE_MEMBER") == null) {
+            roleService.save(new Role("ROLE_MEMBER"));
+        }
 
         // Admin account
-//        if (userService.findByEmail("admin@gmail.com") == null) {
-//            User admin = new User();
-//            admin.setEmail("admin@gmail.com");
-//            admin.setPassword(passwordEncoder.encode("123456"));
-//            HashSet<Role> roles = new HashSet<>();
-//            roles.add(roleService.findByName("ROLE_ADMIN"));
-//            roles.add(roleService.findByName("ROLE_MEMBER"));
-//            admin.setRoles(roles);
-//            userService.save(admin);
-//        }
+        if (userService.findByEmail("admin@gmail.com") == null) {
+            User admin = new User();
+            admin.setEmail("admin@gmail.com");
+            admin.setPassword(passwordEncoder.encode("123456"));
+            HashSet<Role> roles = new HashSet<>();
+            roles.add(roleService.findByName("ROLE_ADMIN"));
+            roles.add(roleService.findByName("ROLE_MEMBER"));
+            admin.setRoles(roles);
+            userService.save(admin);
+        }
 
         // Member account
-//        if (userService.findByEmail("member@gmail.com") == null) {
-//            User user = new User();
-//            user.setEmail("member@gmail.com");
-//            user.setPassword(passwordEncoder.encode("123456"));
-//            HashSet<Role> roles = new HashSet<>();
-//            roles.add(roleService.findByName("ROLE_MEMBER"));
-//            user.setRoles(roles);
-//            userService.save(user);
-//        }
+        if (userService.findByEmail("member@gmail.com") == null) {
+            User user = new User();
+            user.setEmail("member@gmail.com");
+            user.setPassword(passwordEncoder.encode("123456"));
+            HashSet<Role> roles = new HashSet<>();
+            roles.add(roleService.findByName("ROLE_MEMBER"));
+            user.setRoles(roles);
+            userService.save(user);
+        }
 
     }
 }

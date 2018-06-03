e013dbbf5b92ebdc533659f2d31da578a40cf7d3
nguyenvantra <dkdarknessnvt@gmail.com>
2018-06-03 19:42:40 +0700

    Update repository for application

--- springboot-springsecurity-jpa-sql/src/main/java/com/darkness/repository/RoleRepository.java
+++ springboot-springsecurity-jpa-sql/src/main/java/com/darkness/repository/RoleRepository.java
@@ -2,13 +2,9 @@ package com.darkness.repository;
 
 import com.darkness.entity.Role;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-/**
- * @author tranguyenvan
- * @version 1.0.0
- * @since 1.0.0
- * @description
- * */
+@Repository
 public interface RoleRepository extends CrudRepository<Role, Integer> {
     Role findByName(String name);
 }

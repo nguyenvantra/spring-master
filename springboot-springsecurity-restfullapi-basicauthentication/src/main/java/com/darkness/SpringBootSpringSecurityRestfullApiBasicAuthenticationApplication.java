b48f8ed4a7b87613879dc7932a77c8488ec27366
nguyenvantra <dkdarknessnvt@gmail.com>
2018-06-11 22:24:35 +0700

    init project

---
+++ springboot-springsecurity-restfullapi-basicauthentication/src/main/java/com/darkness/SpringBootSpringSecurityRestfullApiBasicAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.darkness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringSecurityRestfullApiBasicAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSpringSecurityRestfullApiBasicAuthenticationApplication.class, args);
+    }
+}
